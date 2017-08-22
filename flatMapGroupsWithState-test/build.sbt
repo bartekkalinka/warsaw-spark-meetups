da20e0c63d7a5c254196acff0da4db4249e87ac9
Bartek Kalinka <bartkali@hotmail.com>
2017-08-22 20:44:21 +0200

    kafka

--- flatMapGroupsWithState-test/build.sbt
+++ flatMapGroupsWithState-test/build.sbt
@@ -4,7 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-sql" % "2.2.0"
+)
 
 mainClass := Some("Main")
     
--- flatMapGroupsWithState-test/src/main/scala/Main.scala
+++ flatMapGroupsWithState-test/src/main/scala/Main.scala
@@ -8,6 +8,19 @@ object Main {
 
     val ds = spark.range(3).withColumn("group", $"id" % 2).as[(Long, Long)].groupByKey { case (id, g) => g }
     ds.flatMapGroupsWithState[Long, (Long, Long)](OutputMode.Append(), GroupStateTimeout.NoTimeout()) { case (a, iter, state) => iter }
+
+    val wordsGrouped = kafka(spark)
+    wordsGrouped.flatMapGroupsWithState[Long, (String, Long)](OutputMode.Append(), GroupStateTimeout.NoTimeout()) { case (word, iter, state) =>
+      val count = state.getOption.getOrElse(0L) + iter.length
+      state.update(count)
+      Iterator((word, count))
+    }
+  }
+
+  private def kafka(spark: SparkSession): KeyValueGroupedDataset[String, String] = {
+    import spark.implicits._
+    val fromKafka = spark.readStream.format("kafka").option("subscribe", "sparkathon").option("kafka.bootstrap.servers", "localhost:9092").load
+    fromKafka.select($"value" cast "string" as "line").as[String].flatMap(line => line.split("\\s+")).groupByKey(identity)
   }
 }
 
